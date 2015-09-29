74971e7322b5e004c999ea730ee8de79fab9a0f4
Mahmoud Mheisen <mahmoudmheisen91@gmail.com>
2015-09-29 23:04:40 +0200

    Add header file that include all Stanford library

---
+++ src/StanfordCPPLib.hpp
@@ -0,0 +1,86 @@
+/*
+ * StanfordCPPLib.hpp
+ *
+ *  Created on: Sep 29, 2015
+ *      Author: Mahmoud Mheisen
+ */
+
+#ifndef SRC_STANFORDCPPLIB_HPP_
+#define SRC_STANFORDCPPLIB_HPP_
+
+#include "../StanfordCPPLib/set.h"
+#include "../StanfordCPPLib/simpio.h"
+#include "../StanfordCPPLib/shuffle.h"
+#include "../StanfordCPPLib/sound.h"
+#include "../StanfordCPPLib/sparsegrid.h"
+#include "../StanfordCPPLib/stack.h"
+#include "../StanfordCPPLib/strlib.h"
+#include "../StanfordCPPLib/vector.h"
+//#include "../StanfordCPPLib/gbufferedimage.h"
+//#include "../StanfordCPPLib/gevents.h"
+#include "../StanfordCPPLib/gfilechooser.h"
+//#include "../StanfordCPPLib/ginteractors.h"
+#include "../StanfordCPPLib/gmath.h"
+//#include "../StanfordCPPLib/gobjects.h"
+#include "../StanfordCPPLib/goptionpane.h"
+#include "../StanfordCPPLib/graph.h"
+#include "../StanfordCPPLib/grid.h"
+#include "../StanfordCPPLib/gtimer.h"
+#include "../StanfordCPPLib/gtypes.h"
+//#include "../StanfordCPPLib/gwindow.h"
+#include "../StanfordCPPLib/dawglexicon.h"
+#include "../StanfordCPPLib/deque.h"
+#include "../StanfordCPPLib/direction.h"
+#include "../StanfordCPPLib/base64.h"
+#include "../StanfordCPPLib/basicgraph.h"
+#include "../StanfordCPPLib/bitstream.h"
+#include "../StanfordCPPLib/compare.h"
+//#include "../StanfordCPPLib/console.h"
+#include "../StanfordCPPLib/error.h"
+#include "../StanfordCPPLib/exceptions.h"
+#include "../StanfordCPPLib/filelib.h"
+#include "../StanfordCPPLib/foreach.h"
+#include "../StanfordCPPLib/hashcode.h"
+#include "../StanfordCPPLib/hashmap.h"
+#include "../StanfordCPPLib/hashset.h"
+#include "../StanfordCPPLib/lexicon.h"
+#include "../StanfordCPPLib/linkedlist.h"
+#include "../StanfordCPPLib/map.h"
+#include "../StanfordCPPLib/observable.h"
+//#include "../StanfordCPPLib/platform.h"
+#include "../StanfordCPPLib/point.h"
+#include "../StanfordCPPLib/pqueue.h"
+#include "../StanfordCPPLib/queue.h"
+#include "../StanfordCPPLib/random.h"
+#include "../StanfordCPPLib/regexpr.h"
+#include "../StanfordCPPLib/timer.h"
+#include "../StanfordCPPLib/tokenscanner.h"
+#include "../StanfordCPPLib/urlstream.h"
+
+#endif /* SRC_STANFORDCPPLIB_HPP_ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
