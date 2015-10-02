fc5dd5157798c0a74bede4c11d2c39670949e393
Mahmoud Mheisen <mahmoudmheisen91@gmail.com>
2015-10-02 12:36:40 +0200

    Add chapter three problems

---
+++ src/chapterThreeProblems.cpp
@@ -0,0 +1,103 @@
+/*
+ * chapterThreeProblems.cpp
+ *
+ *  Created on: Oct 2, 2015
+ *      Author: Mahmoud Mheisen
+ */
+
+/*
+* File: chapterThreeProblems.cpp
+* ------------------
+* This file contain chapter two problems from
+* Programming Abstractions in C++, Eric S. Roberts and Julie Zelenski
+*/
+#include "chapterThreeProblems.hpp"
+
+
+// TODO:
+/*
+ * Function: clearComments
+ * Usage: clearComments(input, output);
+ * -------------------------------------
+ * This function copy the input file and remove the comments from it
+ * and put it in the output file
+ */
+void clearComments(std::ifstream& input, std::ofstream& output) {
+	char first, second;
+	bool commentFlag = false;
+
+	while ((first = input.get()) != EOF) {
+		if (commentFlag) {
+			if (first == '*') {
+				second = input.get();
+				if (second == '/')
+					commentFlag = false;
+				else
+					input.unget();
+			}
+		}
+		else {
+			if (first == '/') {
+				second = input.get();
+				if (second == '*')
+					commentFlag = true;
+				else
+					input.unget();
+			}
+			if (!commentFlag)
+				output.put(first);
+		}
+	}
+}
+
+/*
+ * Main program
+ * ------------
+ * This program will init a deck of cards, shuffle and
+ * display them
+ */
+void cardApp(void) {
+	CardT deck[52];
+
+	// init:
+	int j = 0;
+	for(int rank = 1; rank <= 13; rank++) {
+		for(int suit = 1; suit <= 4; suit++) {
+			deck[j++] = CardT(RankT(rank), SuitT(suit));
+		}
+	}
+
+	// shuffle:
+	for(int i = 0; i < 52; i++) {
+		int newPos = randomInteger(i, 51);
+		CardT temp = deck[i];
+		deck[i] = deck[newPos];
+		deck[newPos] = temp;
+	}
+
+	// display:
+	for(int i = 0; i < 52; i++) {
+		cout << setw(4) << deck[i].toString();
+		if((i+1) % 13 == 0) cout << endl;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/chapterThreeProblems.hpp
@@ -0,0 +1,27 @@
+/*
+ * chapterThreeProblems.hpp
+ *
+ *  Created on: Oct 2, 2015
+ *      Author: Mahmoud Mheisen
+ */
+
+#ifndef SRC_CHAPTERTHREEPROBLEMS_HPP_
+#define SRC_CHAPTERTHREEPROBLEMS_HPP_
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <cmath>
+
+#include "../StanfordCPPLib/simpio.h"
+#include "../StanfordCPPLib/error.h"
+#include "../StanfordCPPLib/random.h"
+#include "Card.hpp"
+using namespace std;
+
+/* functions prototypes */
+void clearComments(std::ifstream& input, std::ofstream& output);
+void cardApp(void);
+
+#endif /* SRC_CHAPTERTHREEPROBLEMS_HPP_ */
