5b4e6635b1b1232a132e9b6afdf09c9325f17b66
Mahmoud Mheisen <mahmoudmheisen91@gmail.com>
2015-10-04 22:13:18 +0200

    Ignore main

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,19 +5,67 @@
  *      Author: Mahmoud Mheisen
  */
 
-#include <iostream>
-using namespace std;
+#include "chapterOneCodes.hpp"
+#include "chapterOneProblems.hpp"
+#include "numToEnglishTextApp.hpp"
+#include "chapterTwoProblems.hpp"
+#include "chapterThreeProblems.hpp"
+#include "chapterFourProblems.hpp"
+#include "chapterFiveProblems.hpp"
+#include "chapterSixProblems.hpp"
 
-#include "../StanfordCPPLib/vector.h"
-
+/* main for all code */
 int main(void) {
 
-	Vector<int> vec;
-	vec.push_back(3);
-	vec.push_back(2);
+	//runSimulation();
+	//checkSpelling();
+	//countWordFrequency();
+	//morseCode();
+	//interpreter();
+	//moveTower(3, 'A', 'B', 'C');
+	//listPermutations("ABC");
 
-	cout << "vec = " << vec.toString() << endl;
+	/*GWindow app(600, 400);
+	app.center();
+
+	GLine line(150, 180, 30, 50);
+	line.setColor("red");
+	line.setLineWidth(5);
+
+	GRect rect(300, 200, 50, 50);
+	rect.setColor("blue");
+	rect.setLineWidth(3);
+
+	app.draw(line);
+	app.draw(rect);*/
+
+	//mondrianPaint();
+	//KochFractal(250, 9);
+	//cout << numberOfHanoiMoves(10);
+	//moveTower2(3, 'A', 'B', 'C');
+	generateGrayCode(2);
 
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
